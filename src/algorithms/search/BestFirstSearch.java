1540f5b6de78b86c67ac3b8e5953ce3aaf071567
HadarSabati <75303359+HadarSabati@users.noreply.github.com>
2021-04-02 15:18:46 +0300

    init Assinment B and BFS

---
+++ src/algorithms/search/BestFirstSearch.java
@@ -0,0 +1,13 @@
+package algorithms.search;
+
+public class BestFirstSearch extends ASearchingAlgorithm{
+    @Override
+    public int getNumberOfVisitedNodes() {
+        return super.getNumberOfVisitedNodes();
+    }
+
+    @Override
+    public AState search(ISearchable s) {
+        return super.search(s);
+    }
+}
