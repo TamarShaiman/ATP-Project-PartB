42ec8730ae8ae74302a2fa42d402a6020ca3a319
HadarSabati <75303359+HadarSabati@users.noreply.github.com>
2021-04-27 18:09:31 +0300

    init assignment A

--- src/IO/SimpleDecompressorInputStream.java
+++ src/IO/SimpleDecompressorInputStream.java
@@ -14,4 +14,33 @@ public class SimpleDecompressorInputStream extends InputStream {
     public int read() throws IOException {
         return 0;
     }
+
+    @Override
+    public int read(byte b[]) throws IOException {
+        int resInd = 0;
+        int inInd = 0;
+        int size = 0;
+        byte[] input = in.readAllBytes();
+        for (int j = 0; j < input.length; j++) {
+            size += (int)input[j];
+        }
+        byte[] res = new byte[size];
+
+        for (int j = 0; j < input.length; j++) {
+            for (int i = 0; i < input[j]; i++) {
+                if (j % 2 == 0) {
+                    res[resInd] = 0;
+                }
+                else{
+                    res[resInd] = 1;
+                }
+                resInd++;
+            }
+        }
+        b = res;
+        return read(b, 0, b.length);
+    }
+
+
+
 }
