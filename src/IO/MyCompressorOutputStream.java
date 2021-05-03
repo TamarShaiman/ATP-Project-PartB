323b33c249c61bc5b5899abfb22e21dac2b2f020
HadarSabati <75303359+HadarSabati@users.noreply.github.com>
2021-05-03 18:56:05 +0300

    MyCompressorOutputStream

--- src/IO/MyCompressorOutputStream.java
+++ src/IO/MyCompressorOutputStream.java
@@ -12,7 +12,30 @@ public class MyCompressorOutputStream extends OutputStream {
 
     @Override
     public void write(int b) throws IOException {
-        int count = 0;
 
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        int count = 0;
+        int resInd = 0;
+        int newSize = Math.round(b.length/8);
+        byte[] res = new byte[newSize];
+        for (int i = 0; i < b.length; i = i+8) {
+            byte newNum = getBinToDec(b, i);
+            res[resInd] = newNum;
+            resInd++;
+        }
+        out.write(res);
+    }
+
+    private byte getBinToDec(byte[] b, int i) {
+        int sum = 0;
+        for (int j = 7; j > 0; j--) {
+            if (b[i+j] == (byte)1){
+                sum += Math.pow(2, j);
+            }
+        }
+        return (byte)sum;
     }
 }
